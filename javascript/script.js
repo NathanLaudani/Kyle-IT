b6ac48b00650d42eb8955ae85faee16015c43d2d
NathanLaudani <nathanlaudani@stmarksschool.org>
2016-05-06 11:31:11 -0400

    updated 5 6

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,33 @@
 
-$("#slideshow > div:gt(0)").hide();
+$(document).ready(function() {
+  $('ol').sortable();
+    //$('ol').selectable();
+});
 
-setInterval(function() {
-    $('#slideshow > div:first')
-        .fadeOut(1000)
-        .next()
-        .fadeIn(1000)
-        .end()
-        .appendTo('#slideshow');
-},  3000);
+$(function () {
+
+    /* SET PARAMETERS */
+    var change_img_time     = 5000; 
+    var transition_speed    = 100;
+
+    var simple_slideshow    = $("#exampleSlider"),
+        listItems           = simple_slideshow.children('li'),
+        listLen             = listItems.length,
+        i                   = 0,
+
+        changeList = function () {
+
+            listItems.eq(i).fadeOut(transition_speed, function () {
+                i += 1;
+                if (i === listLen) {
+                    i = 0;
+                }
+                listItems.eq(i).fadeIn(transition_speed);
+            });
+
+        };
+
+    listItems.not(':first').hide();
+    setInterval(changeList, change_img_time);
+
+});
